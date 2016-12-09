c878ec150b59b0288df78ee840aabce0e93eb819
Colin Burgin <cburgin707@gmail.com>
2016-12-09 17:26:01 -0500

    Basics testing says it runs...

--- cache.py
+++ cache.py
@@ -8,12 +8,12 @@ class cache:
         self.blockSize = blockSize
 
         #Calculate internal cache setup
-        self.numBlocks = self.cacheSize / self.blockSize
+        self.numBlocks = int(self.cacheSize / self.blockSize)
         if mapping != 0:
             self.cacheMap = mapping
         else:
             self.cacheMap = self.numBlocks
-        self.numSets = self.cacheSize / (self.cacheMap * self.blockSize)
+        self.numSets = int(self.cacheSize / (self.cacheMap * self.blockSize))
 
         #Create a 2D array of cacheline elements
         cacheLine = {'valid':0, 'tag':0}
@@ -23,7 +23,7 @@ class cache:
         self.lru = [[0 for x in range(self.cacheMap)] for y in range(self.numSets)]
 
     #Accepts a single atomic operation and runs it through the cache.
-    def simulate_element(element):
+    def simulate_element(self, element):
         cacheHit = False
 
         #Read in the elements r/w and address
@@ -31,82 +31,82 @@ class cache:
         address = element[1]
 
         #Find the set and tag based on the input address
-        curr_set = (address / self.blockSize) % self.numSets
-        curr_tag = (address / self.blockSize) / self.numSets
+        curr_set = int((address / self.blockSize) % self.numSets)
+        curr_tag = int((address / self.blockSize) / self.numSets)
 
         #Increment every location in the LRU every time there is a cache access
-        for i in range(len(self.numSets)):
-            for j in range(len(self.cacheMap)):
+        for i in range(self.numSets):
+            for j in range(self.cacheMap):
                 self.lru[i][j] += 1
 
         if read_write == 'read':
             found = False
-            for i in range(len(self.cacheMap)):
-                if self.cache[curr_set][i]['tag'] == curr_tag && self.cache[curr_set][i]['valid'] != 0:
+            for i in range(self.cacheMap):
+                if self.cache[curr_set][i]['tag'] == curr_tag and self.cache[curr_set][i]['valid'] != 0:
                     #If the tag matches and the data is valid then mark as found
                     #and as a hit and reset the LRU in this location.
                     cacheHit = True
                     found = True
-                    lru[curr_set][i] = 0
+                    self.lru[curr_set][i] = 0
 
-            if !found:
+            if not found:
                 index = 0
                 #Need to pull data into the cache, look for invalid locations
                 #before using the lru to evict someone
-                for j in range(len(self.cacheMap)):
-                    if self.cache[curr_set][j]['valid'] == 0 && !found:
+                for j in range(self.cacheMap):
+                    if self.cache[curr_set][j]['valid'] == 0 and not found:
                         #Loop until and invalid location is found.  Once found
                         #set found to true, reset the lru and mark the index that
                         #it was found in.
                         found = True
-                        lru[curr_set][j] = 0
+                        self.lru[curr_set][j] = 0
                         index = j
 
                 #If there aren't any invalid locations then use the lru
-                if !found:
-                    for j in range(len(self.cacheMap)):
+                if not found:
+                    for j in range(self.cacheMap):
                         if self.lru[curr_set][j] > self.lru[curr_set][index]:
                             index = j
 
                     #Reset the lru in the location choosen to be replaced
-                    lru[curr_set][index] = 0
+                    self.lru[curr_set][index] = 0
 
                 #Update the cache information
-                self.myCache[curr_set][index]['tag'] = curr_tag
-                self.myCache[curr_set][index]['valid'] = 1
+                self.cache[curr_set][index]['tag'] = curr_tag
+                self.cache[curr_set][index]['valid'] = 1
 
         elif read_write == 'write':
             found = False
             index = 0
-            for i in range(len(self.cacheMap)):
-                if self.cache[curr_set][i]['tag'] == curr_tag && !found:
+            for i in range(self.cacheMap):
+                if self.cache[curr_set][i]['tag'] == curr_tag and not found:
                     #If the tag matches and it has not been found yet
                     cacheHit = True
                     found = True
-                    lru[curr_set][i] = 0
+                    self.lru[curr_set][i] = 0
                     index = i
 
-            if !found:
-                for i in range(len(self.cacheMap)):
-                    if self.cache[curr_set][i]['valid'] == 0 && !found:
+            if not found:
+                for i in range(self.cacheMap):
+                    if self.cache[curr_set][i]['valid'] == 0 and not found:
                         #Loop until and invalid location is found.  Once found
                         #set found to true, reset the lru and mark the index that
                         #it was found in.
                         found = True
-                        lru[curr_set][i] = 0
+                        self.lru[curr_set][i] = 0
                         index = i
 
                 #If there aren't any invalid locations then use the lru
-                if !found:
-                    for i in range(len(self.cacheMap)):
+                if not found:
+                    for i in range(self.cacheMap):
                         if self.lru[curr_set][i] > self.lru[curr_set][index]:
                             index = i
 
                     #Reset the lru in the location choosen to be replaced
-                    lru[curr_set][index] = 0
+                    self.lru[curr_set][index] = 0
 
                 #Update the cache information
-                self.myCache[curr_set][index]['tag'] = curr_tag
-                self.myCache[curr_set][index]['valid'] = 1
+                self.cache[curr_set][index]['tag'] = curr_tag
+                self.cache[curr_set][index]['valid'] = 1
 
         return cacheHit
