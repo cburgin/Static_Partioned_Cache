6b32cec96abc4670accee0a475739da872ead248
Colin Burgin <cburgin707@gmail.com>
2016-12-09 20:24:27 -0500

    Working on argparse.

---
+++ simulate.py
@@ -0,0 +1,10 @@
+#Simulates the entire system
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Simulates the static partioned cache.')
+    parser.add_argument()
+
+if __name__ = ='__main__':
+    main()
